f6ed75a9b4856aa98585f8e49db88c2da11ba51e
Sławek <spytlarz@jasfbg.pl>
2023-10-06 12:54:34 +0200

    Add project

---
+++ src/main/java/pl/slawek/Garden/Menager/GardenMenagerApplication.java
@@ -0,0 +1,13 @@
+package pl.slawek.Garden.Menager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenMenagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenMenagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/slawek/Garden/Menager/GardenMenagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.slawek.Garden.Menager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenMenagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
