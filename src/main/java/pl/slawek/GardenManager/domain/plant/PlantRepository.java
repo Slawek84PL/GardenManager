750bafacd21b632122b71480fab119082bd2c1aa
Slawek <slapyt84@gmail.com>
2023-10-31 06:26:10 +0100

    Added test for BaseEntity

---
+++ src/main/java/pl/slawek/GardenManager/domain/plant/PlantRepository.java
@@ -0,0 +1,8 @@
+package pl.slawek.GardenManager.domain.plant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlantRepository extends JpaRepository<Plant, Long> {
+}
