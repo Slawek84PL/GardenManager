239d0f13c76a928b3c092aa279a90c4366c52a1b
Slawek <slapyt84@gmail.com>
2023-10-31 05:38:09 +0100

    Changed main package name

--- src/main/java/pl/slawek/GardenManager/GardenManagerApplication.java
+++ src/main/java/pl/slawek/GardenManager/GardenManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GardenMenagerApplication {
+public class GardenManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GardenMenagerApplication.class, args);
+		SpringApplication.run(GardenManagerApplication.class, args);
 	}
 
 }
