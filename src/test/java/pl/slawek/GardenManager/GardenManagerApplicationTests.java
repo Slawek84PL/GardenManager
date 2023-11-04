07f2106cff983222e135378e879610daa4fb850f
Slawek <slapyt84@gmail.com>
2023-11-04 23:12:52 +0100

    Changed name

--- src/test/java/pl/slawek/GardenManager/GardenManagerApplicationTests.java
+++ src/test/java/pl/slawek/GardenManager/GardenManagerApplicationTests.java
@@ -1,10 +1,10 @@
-package pl.slawek.GardenMenager;
+package pl.slawek.GardenManager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GardenMenagerApplicationTests {
+class GardenManagerApplicationTests {
 
 	@Test
 	void contextLoads() {
