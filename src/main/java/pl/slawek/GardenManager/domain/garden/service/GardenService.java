2287f155aaf991f67694d8e69866ac2b33ff8bb3
Slawek <slapyt84@gmail.com>
2023-11-08 12:54:11 +0100

    Refactored GardenService

--- src/main/java/pl/slawek/GardenManager/domain/garden/service/GardenService.java
+++ src/main/java/pl/slawek/GardenManager/domain/garden/service/GardenService.java
@@ -1,7 +1,46 @@
 package pl.slawek.GardenManager.domain.garden.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.slawek.GardenManager.domain.garden.Garden;
+import pl.slawek.GardenManager.domain.garden.repository.GardenRepository;
+
+import java.util.List;
 
 @Service
 public class GardenService {
+
+    private final GardenRepository repository;
+
+    public GardenService(GardenRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public Garden create(Garden garden) {
+        return repository.save(garden);
+    }
+
+    @Transactional(readOnly = true)
+    public Garden garden(Long id) {
+        return repository.getReferenceById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Garden> gardens() {
+        return repository.findAll();
+    }
+
+    @Transactional
+    public Garden update(Long id, Garden gardenRequest) {
+        Garden garden = repository.getReferenceById(id);
+        garden.setName(gardenRequest.getName());
+
+        return garden;
+    }
+
+    @Transactional
+    public void delete(Garden garden) {
+        repository.delete(garden);
+    }
 }
