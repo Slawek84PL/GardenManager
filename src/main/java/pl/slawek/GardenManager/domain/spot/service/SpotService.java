d32f22526da66cfcc760af874954472bcab4efa4
Slawek <slapyt84@gmail.com>
2023-11-08 08:14:50 +0100

    Refactored SpotService

--- src/main/java/pl/slawek/GardenManager/domain/spot/service/SpotService.java
+++ src/main/java/pl/slawek/GardenManager/domain/spot/service/SpotService.java
@@ -10,21 +10,37 @@ import java.util.List;
 @Service
 public class SpotService {
 
+    private final SpotRepository repository;
+
     public SpotService(SpotRepository repository) {
+        this.repository = repository;
     }
 
+    @Transactional
     public Spot create(Spot spot) {
+        return repository.save(spot);
     }
 
+    @Transactional(readOnly = true)
     public Spot spot(Long id) {
+        return repository.getReferenceById(id);
     }
 
+    @Transactional(readOnly = true)
     public List<Spot> spots() {
+        return repository.findAll();
     }
 
+    @Transactional
     public Spot update(Long id, Spot spotRequest) {
+        Spot spot = repository.getReferenceById(id);
+        spot.setName(spotRequest.getName());
+
+        return spot;
     }
 
+    @Transactional
     public void delete(Spot spot) {
+        repository.delete(spot);
     }
 }
